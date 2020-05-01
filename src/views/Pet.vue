3e89387d9d92048ff51367ed5b9b153de64fe871
Mohamed LOTF <mohamed.lotf@outlook.com>
2020-05-01 16:16:16 +0200

    all adopt pets project files

---
+++ src/views/Pet.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>{{ animal.name }}</h1>
+      <h2>{{ animal.gender }}</h2>
+      <h3>{{ animal.age }}</h3>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
